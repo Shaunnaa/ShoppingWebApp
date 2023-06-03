e86f385115729aebe013166389feffde0fbe95e8
Harith Phalangpatanakij <sornharith@Hariths-MacBook-Pro.local>
2023-06-03 23:41:37 +0700

    adding api to finding user by using name

--- Back-end/routers/user.py
+++ Back-end/routers/user.py
@@ -31,17 +31,23 @@ async def get_users():
     return user_db
 
 
-@router.get("/{user_id}")
-async def get_user(user_id: int):
+@router.get("/{user_id}") # get user by id
+async def get_user_id(user_id: int):
     return user_db[user_id-1]
 
-@router.post("/")
+@router.get("/name/{user_name}") # get user by name
+async def get_user_name(user_name: str):
+    for user in user_db:
+        if (user['name'] == user_name):
+            return user
+
+@router.post("/") #create new user
 async def create_user(user: User):
     result = user.dict()
     user_db.append(result)
     return user_db[-1]
 
-@router.put("/{user_id}")
+@router.put("/{user_id}") # edit user information by id
 async def edit_user(user_id: int, user: User):
     result = user.dict()
     user_db[user_id-1].update(result)
