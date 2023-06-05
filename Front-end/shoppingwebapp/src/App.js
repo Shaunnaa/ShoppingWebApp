2ee218e92b30596e293d0617db63bbbd3d958cef
Harith Phalangpatanakij <sornharith@Hariths-MacBook-Pro.local>
2023-06-05 22:57:53 +0700

    adding login and signup

--- Front-end/shoppingwebapp/src/App.js
+++ Front-end/shoppingwebapp/src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import { BrowserRouter as Router,Route, Switch} from 'react-router-dom';
 import Home from './Pages/Cart.js/Home';
 import All from './All';
+import './login.css';
+import {Login} from './Login';
 function App() {
   return (
     <Router>
-      
       <div className="content">
         <Switch>
           <Route exact path="/">
@@ -16,6 +17,10 @@ function App() {
             <Home/>
           </Route>
         </Switch>
+      </div>
+
+      <div className="App">
+        <Login />
       </div>
     </Router>
     
---
+++ Front-end/shoppingwebapp/src/Login.jsx
@@ -0,0 +1,39 @@
+import React,{ useState} from "react";
+import img1 from './img/shop_1.png';
+import img2 from './img/user_1.png';
+import img3 from './img/pass_1.png';
+
+
+export const Login = (props) =>{
+    const [email,setEmail]=useState ('');  
+    const [pass,setPass]=useState('');
+    
+    const handleSubmit =(e) =>{
+        e.preventDefault();
+        console.log(email);
+    }
+    return(
+        
+        <div className="form-container">
+            
+            <button className="button-signup">Sign up</button>
+            <button className="button-login">Login</button>
+        <form className="Login-email" onSubmit={handleSubmit}>
+            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" required placeholder=" Username/Email       " id="email" name="email"/>
+        </form>
+
+
+        <form className="Login-pass" onSubmit={handleSubmit}>
+            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" required placeholder=" Password              " id="password" name="password"/>
+            
+        </form>
+
+            <button className="button-login-down" type="submit">Login</button>
+
+            <button className="link-forget" > Forgot Password? </button>
+            <img className="shop1" src={img1} alt="shop1"></img>
+            <img className="user1" src={img2} alt="user1"></img>
+            <img className="pass1" src={img3} alt="pass1"></img>
+        </div>
+    )
+}
