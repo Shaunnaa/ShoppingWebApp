8795ea0438f8577e730f10fbd936e8c30ad5df0c
Shaunnaa <107536959+Shaunnaa@users.noreply.github.com>
2023-06-06 00:19:57 +0700

    Model updat

--- Back-end/models.py
+++ Back-end/models.py
@@ -1,15 +1,15 @@
 #from tortoise import Model
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 import datetime as dt
 from datetime import datetime
 
 
 class items(BaseModel):
     id = int
-    name: str
+    name: str = Field(..., max_length=50)
     category: str
     description: str
-    original_price: float
+    original_price: float 
     discount_price: float
     percent_discount: int
     offer_expiration_date: datetime = datetime.utcnow()
